62c8fdf21367e89d1eb3feccdc59b7b407a319b0
Majid Askarifarsangi <maf256@gmail.com>
2025-03-19 18:15:37 +0100

    update SEO

--- next.config.js
+++ next.config.js
@@ -1,32 +1,3 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   babel: {
-//     plugins: [['styled-components', { ssr: true }]],
-//   },
-//   reactStrictMode: true,
-//   output: 'standalone', // Added missing comma
-//   async headers() {
-//     // Moved this to align properly in the object
-//     return [
-//       {
-//         source: '/(.*)',
-//         headers: [
-//           {
-//             key: 'X-Robots-Tag',
-//             value: 'noarchive',
-//           },
-//           {
-//             key: 'Content-Rating',
-//             value: 'General',
-//           },
-//         ],
-//       },
-//     ]
-//   },
-// }
-
-// module.exports = nextConfig
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
